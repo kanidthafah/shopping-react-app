3182449f76f7fa9e044bba920bc0c7af092a5688
kanidthafah <kanidtha.fah@gmail.com>
2024-01-18 18:35:05 +0700

    edit code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,19 +11,7 @@ function Navbar(){
     const toggleMenu = () => {
         setOpenMenu(!openMenu)
     }
-
-    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem("cart")) || [])
   
-    useEffect(() => {
-        //fetch cart item from localStorage
-        try {
-            const storedCartItems = cartItems
-            setCartItems(storedCartItems)
-            } catch (err) {
-            console.log('Error: ', err.message);
-          }
-    }, [])
-
     return (
         <div className="max-w-full container sticky top-0 z-10">
             <nav className="h-12 flex justify-between items-center shadow bg-white px-4 md:px-10">
@@ -54,11 +42,7 @@ function Navbar(){
                 {/* MENU ICONS */}
                 <div className="flex justify-center items-center gap-x-2 text-2xl text-Neutral1">
 
-                    <div className="relative px-4 py-2">
-                        <div className="absolute right-2 top-1 bg-red-600 text-white text-xs px-1 rounded-md">
-                            <span>{cartItems.length}</span>
-                        </div>
-
+                    <div className="px-2 py-2">
                         <Link to="/cart"className="hover:text-Sky">
                             <IoCart  />
                         </Link>
